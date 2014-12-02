68a6b14924c27acfd88e0555a07e3b1246975a73
George Meyer <gmeyer@flvs.net>
2014-12-02 18:34:57 -0500

    initial working alpha version of project

---
+++ js/jsSeleniumBuilder.js
@@ -0,0 +1,6 @@
+var jsSeleniumBuilder = {
+
+    "makeCommand" : function(c,t,v){  //command,target, value: c,t,v
+               return "<tr><td>"+c+"</td><td>"+t+"</td><td>"+v+"</td></tr>";
+    }
+}
